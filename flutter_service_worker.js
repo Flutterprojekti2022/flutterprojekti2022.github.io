49997f041c23a40d1a030b64ec386d204892ef92
enpassant <>
2022-02-21 13:48:54 +0200

    new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "79f1fe64721eeb52c37615d9cead6b56",
-"index.html": "04c4b8ca9a208c8c8b7d9d5af6ca5d6e",
-"/": "04c4b8ca9a208c8c8b7d9d5af6ca5d6e",
+  "main.dart.js": "3a34be2fff0c64fab3a3561cd819abb5",
+"index.html": "90c6aec790ec73bf5a17020286e6efbf",
+"/": "90c6aec790ec73bf5a17020286e6efbf",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
