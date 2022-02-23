cb38c699d0d26efae53d83137bb216ed6d9a1d82
ziptyr <>
2022-02-23 16:36:51 +0200

    w/ hamburgermenu and fake data, w/o firebase

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "3a34be2fff0c64fab3a3561cd819abb5",
-"index.html": "90c6aec790ec73bf5a17020286e6efbf",
-"/": "90c6aec790ec73bf5a17020286e6efbf",
+  "main.dart.js": "c6713997bd0789632dd7d2889c5b5896",
+"index.html": "32586a5b4e855e64cfe83865f5d657df",
+"/": "32586a5b4e855e64cfe83865f5d657df",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
